c9c5a64218f3a48cf82f864a0a2ddd69aec67f63
Gokul <krishg4@rpi.edu>
2019-03-23 11:54:45 -0400

    Created SQL statements for more specialized searches

--- get_tables.php
+++ get_tables.php
@@ -11,7 +11,27 @@ $client = $_POST['client'];
 $branch = $_POST['branch'];
 $keyword = $_POST['keyword'];
 
-$stmt = $pdo->query("SELECT * FROM RCOS_APL.Client where client_id = ". $client .";");
+if (isset($_POST['Perma']) && $_POST['Perma'] == 'Yes'){
+    echo "Outputing all Permenant Incidents involving client ". $client;
+    $stmt = $pdo->query("SELECT * 
+                                   FROM RCOS_APL.Client c, rcos_apl.incident i, rcos_apl.premanent_ban p 
+                                   WHERE (c.client_id = ". $client .") AND
+                                         (c.client_id = i.client_id AND c.pb_id = p.pb_id AND i.pb_id = i.pb_id);");
+}
+else if (isset($_POST['Active']) && $_POST['Active'] == 'Yes'){
+    echo "Outputing all Active Incidents involving client ". $client;
+    $stmt = $pdo->query("SELECT * 
+                                   FROM RCOS_APL.Client c, rcos_apl.incident i, rcos_apl.active_ban p 
+                                   WHERE (c.client_id = ". $client .") AND
+                                         (c.client_id = i.client_id AND c.ab_id = p.ab_id AND i.ab_id = i.ab_id);");
+}
+else{
+    $stmt = $pdo->query("SELECT * 
+                                   FROM RCOS_APL.Client 
+                                   WHERE client_id = ". $client .";");
+    echo "Outputting all Incidents involving client ". $stmt->fetch(PDO::FETCH_ASSOC)['first_name'];
+}
+
 echo '<table border="1">'."<br />";
 while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
     echo "<tr><td>";
