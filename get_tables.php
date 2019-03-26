4f5c72e3cb8cd790f07a065a4922296065a6fc94
Gokul <krishg4@rpi.edu>
2019-03-26 16:50:39 -0400

    Updated get_tables to work with Permananet and Active Bans

--- get_tables.php
+++ get_tables.php
@@ -11,35 +11,71 @@ $client = $_POST['client'];
 $branch = $_POST['branch'];
 $keyword = $_POST['keyword'];
 
+
 if (isset($_POST['Perma']) && $_POST['Perma'] == 'Yes'){
     echo "Outputing all Permenant Incidents involving client ". $client;
     $stmt = $pdo->query("SELECT *
-                                   FROM RCOS_APL.Client c, rcos_apl.incident i, rcos_apl.premanent_ban p
+                                   FROM   APL.Client c, APL.incident i, APL.permanent_ban p
                                    WHERE (c.client_id = ". $client .") AND
-                                         (c.client_id = i.client_id AND c.pb_id = p.pb_id AND i.pb_id = i.pb_id);");
+                                         (c.client_id = i.client_id AND i.pb_id = p.pb_id);");
+
+    echo '<table border="1">'."<br />";
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        echo "<tr><td>";
+        echo($row['first_name']);
+        echo("</td><td>");
+        echo($row['last_name']);
+        echo("</td><td>");
+        echo($row['age']);
+        echo("</td><td>");
+        echo($row['incident_description']);
+        echo("</td><td>");
+        echo($row['pb_description']);
+        echo("</td></tr><br/>");
+    }
+    echo "</table><br/>";
 }
 else if (isset($_POST['Active']) && $_POST['Active'] == 'Yes'){
     echo "Outputing all Active Incidents involving client ". $client;
     $stmt = $pdo->query("SELECT *
-                                   FROM RCOS_APL.Client c, rcos_apl.incident i, rcos_apl.active_ban p
+                                   FROM   APL.Client c, APL.incident i, APL.active_ban p
                                    WHERE (c.client_id = ". $client .") AND
-                                         (c.client_id = i.client_id AND c.ab_id = p.ab_id AND i.ab_id = i.ab_id);");
+                                         (c.client_id = i.client_id AND i.ab_id = p.ab_id);");
+
+    echo '<table border="1">'."<br />";
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        echo "<tr><td>";
+        echo($row['first_name']);
+        echo("</td><td>");
+        echo($row['last_name']);
+        echo("</td><td>");
+        echo($row['age']);
+        echo("</td><td>");
+        echo($row['incident_description']);
+        echo("</td><td>");
+        echo($row['ab_description']);
+        echo("</td></tr><br/>");
+    }
+    echo "</table><br/>";
 }
 else{
+    echo "Outputting all Incidents involving Client ID: " . $client ."\r\n";
     $stmt = $pdo->query("SELECT *
-                                   FROM RCOS_APL.Client
-                                   WHERE client_id = ". $client .";");
-    echo "Outputting all Incidents involving client ". $stmt->fetch(PDO::FETCH_ASSOC)['first_name'];
-}
+                                   FROM  APL.Client c, APL.incident i
+                                   WHERE (c.client_id = ". $client .") AND
+                                         (i.client_id = c.client_id);");
 
-echo '<table border="1">'."<br />";
-while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-    echo "<tr><td>";
-    echo($row['first_name']);
-    echo("</td><td>");
-    echo($row['last_name']);
-    echo("</td><td>");
-    echo($row['age']);
-    echo("</td></tr><br/>");
+    echo '<table border="1">'."<br />";
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        echo "<tr><td>";
+        echo($row['first_name']);
+        echo("</td><td>");
+        echo($row['last_name']);
+        echo("</td><td>");
+        echo($row['age']);
+        echo("</td><td>");
+        echo($row['incident_description']);
+        echo("</td></tr><br/>");
+    }
+    echo "</table><br/>";
 }
-echo "</table><br/>";
