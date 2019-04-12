f6503bf9f2d1f34b724e70ca7a5ed3f37331b815
rosewyz <rosewyz1022@gmail.com>
2019-04-12 16:54:12 -0400

    Update csv_parser.php

--- csv_parser.php
+++ csv_parser.php
@@ -1,13 +1,20 @@
 <?php
     require_once "pdo.php";
 
-    $file = $_FILES['filename'];
-    $csv = array_map("str_getcsv", $file);
-    foreach ($csv as $data){
-        print_r($csv[1]);
-        break;
-//    $sql_insert = "INSERT INTO Client(first_name, last_name, age) VALUES(".$data[0].$data[1].null.")";
-//    $pdo->exec($sql_insert);
+    if (isset($_POST['submit']))
+    {
+        //Import uploaded file to Database
+        $handle = fopen($_FILES['filename']['tmp_name'], "r");
+        while(($data = fgetcsv($handle)) !== FALSE){
+            print_r($data);
+            break;
+//            $sql = "INSERT into client (first_name, last_name) values('".$data[0]."', '".$data[1]."')";
+//            mysqli_query($conn, $sql);
+//            $sql = "INSERT into branch (branch) values('".$data[2]."')";
+//            mysqli_query($conn, $sql) ;
+//            $sql = "INSERT into incident(date_time, incident_description) values ('".$data[3]."', '".$data[4]."')";
+//            mysqli_query($conn, $sql);
 
-}
-//print_r($csv[1]);
+        }
+
+    }
