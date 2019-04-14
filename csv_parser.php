a528450f2c66ccb012c1b7686ef096b11f8a65b9
rosewyz <rosewyz1022@gmail.com>
2019-04-14 02:31:32 -0400

    Add bulk upload function
    only for complete list

--- csv_parser.php
+++ csv_parser.php
@@ -25,16 +25,41 @@
                     $branch_id = $row['branch_id'];
                 }
 
+                // Add ban information
+                $ban = -1;
+                $start_date = date("Y-m-d", strtotime($data[6]));
+                if ($data[7] == "Permanent" || $data[7] == "INDEFINITE"){
+                    $ban = 0;
+                    $sql = "INSERT into RCOS_APL.Permanent_Ban (pb_start_date, pb_description, client_id) 
+                            values ('" . $start_date . "', '" . $data[8] . "', '" . $client_id . "')";
+                    $pdo->query($sql);
+                }
+                elseif ($data[7] != ""){
+                    $ban = 1;
+                    $end_date = date("Y-m-d", strtotime($data[7]));
+                    $sql = "INSERT into RCOS_APL.Active_Ban (ab_start_date, ab_end_date, ab_description, client_id) 
+                            values ('" . $start_date . "', '" . $end_date . "', '" . $data[8] . "', '" . $client_id . "')";
+                    $pdo->query($sql);
+                }
+
                 // Add incident information
                 $date = date("Y-m-d H:i:s", strtotime($data[2].$data[4]));
                 $description = str_replace('\'', '\\\'', $data[5]);
-                $sql = "INSERT into RCOS_APL.incident(date_time, incident_description, client_id, branch_id) 
-                        values ('" . $date . "', '" . $description . "', '" . $client_id . "', '" . $branch_id . "')";
+                if ($ban == 0 ){
+                    $pb_id = $pdo->lastInsertId();
+                    $sql = "INSERT into RCOS_APL.incident(date_time, incident_description, client_id, branch_id, pb_id) 
+                            values ('" . $date . "', '" . $description . "', '" . $client_id . "', '" . $branch_id . "', '" . $pb_id. "')";
+                }
+                elseif ($ban == 1){
+                    $ab_id = $pdo->lastInsertId();
+                    $sql = "INSERT into RCOS_APL.incident(date_time, incident_description, client_id, branch_id, ab_id) 
+                            values ('" . $date . "', '" . $description . "', '" . $client_id . "', '" . $branch_id . "', '" . $ab_id. "')";
+                }
+                else{
+                    $sql = "INSERT into RCOS_APL.incident(date_time, incident_description, client_id, branch_id) 
+                            values ('" . $date . "', '" . $description . "', '" . $client_id . "', '" . $branch_id . "')";
+                }
                 $pdo->query($sql);
-//
-//                if ($data[7] == "Permanent" || $data[7] == "INDEFINITE"){
-//                    
-//                }
             }
         }
         header("Location: success_page.php?insert=success");
